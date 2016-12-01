70b2b61b56ac1729b58b32fb351dfc2cb1d29b06
Ashwin <ashwinoscar@gmail.com>
2016-12-01 18:07:26 +0530

    Base page for user

--- sublist.php
+++ sublist.php
@@ -15,21 +15,33 @@ session_start();
 
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
+<link src="stylesheet/cards.css" type="text/css">
 </head>
 <body>
+
 <?php
 include("userheader.php");
 include("database.php");
-echo "<h2 class=head1> Select Subject to Give Quiz </h2>";
 $rs=mysql_query("select * from mst_subject");
-echo "<table align=center>";
+echo "<br><br><div class=\"card-columns\">";
+
 while($row=mysql_fetch_row($rs))
 {
+    echo "<div class=\"card\"> <div class=\"col-sm-4\">";
 	echo "<tr><td align=center ><a href=showtest.php?subid=$row[0]><font size=4>$row[1]</font></a>";
+	echo '<a href=showtest.php?subid=$row[0]>';
+    echo "<div class=\"card card-block\">";
+    echo "</a>";
+    echo " <h6 class=\"card-subtitle text-muted\">"."$row[3]"."</h6>";
+    echo "<p class=\"card-text\">"."$row[2]"."</p>";
+    echo "<a href=\"#\" class=\"card-link\">Card link</a>  <a href=\"#\" class=\"card-link\">Another link</a><br> <br>";  //adding latest test link here
+    echo "</div>";
+    echo "</div></div>";
+
 
 }
-echo "</table>";
+echo "</div>";
 ?>
+
 </body>
 </html>
